0e6d14dc923783fc743b701125dffc28fb2a3e20
Vindhujavenu <vindhuja@94venu>
2019-04-24 19:51:33 +0530

    Create userfeedback.php

---
+++ android_connection11/userfeedback.php
@@ -0,0 +1,16 @@
+<?php
+include("connection.php");
+$feedback=$_GET['feedback'];
+$lid=$_GET['lid'];
+$response=array();
+$gry=mysql_query("insert into feedback(log_id,feedback,date)values('$lid','$feedback',curdate())");
+if($qry>0)
+{
+	$response['status']="1";
+}
+else
+{
+	$response['status']="0";
+}
+echo json_encode($response);
+?>
