4f9ddf344472b19aa610cc23639babdb93804f9e
Vindhujavenu <vindhuja@94venu>
2019-04-24 19:49:46 +0530

    Update viewfamily.php

--- android_connection/viewfamily.php
+++ android_connection/viewfamily.php
@@ -1,26 +1,35 @@
 <?php
 include("connection.php");
 $response=array();
-$lid=$_GET['lid'];
-$qry=mysql_query("select * from family where log_id='$lid'");
-if(mysql_num_rows($qry)>0)
+$res=mysql_query("select * from family");
+if(mysql_num_rows($res)>0)
 {
 	$response["status"]="1";
 	$response["result"]=array();
-	while($b=mysql_fetch_array($qry))
+	while($res1=mysql_fetch_array($res))
+	
+	
+	
 	{
+										
 		$arr=array();
-		$arr["house_no"]=$res1[1];
-		$arr["house_name"]=$res1[2];
-		$arr["owner_name"]=$res1[3];
-		$arr["annual_income"]=$res1[4];
-		$arr["no_of_members"]=$res1[5];
+		$arr["fid"]=$res1['f_id'];
+		$arr["house_no"]=$res1['house_no'];
+		$arr["house_name"]=$res1['house_name'];
+		$arr["owner_name"]=$res1['owner_name'];
+		$arr["annual_income"]=$res1['annual_income'];
+		$arr["no_of_members"]=$res1['no_of_members'];
+		$arr["pid"]=$res1['pid'];
+		
 		array_push($response["result"],$arr);
-}
+		
+	}
+	
 }
 else
 {
-		$response["status"]="0";
+	$response["status"]="0";
 }
 echo json_encode($response);
+
 ?>
