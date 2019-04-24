17744c18df709cc485db8d6da4b31e3d9a280711
Vindhujavenu <vindhuja@94venu>
2019-04-24 20:08:27 +0530

    Create cheader.php

---
+++ cheader.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Hospice Medical</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="vendors/linericon/style.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+	<link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+	<link rel="stylesheet" href="vendors/animate-css/animate.css">
+	<link rel="stylesheet" href="vendors/jquery-ui/jquery-ui.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+
+
+
+	<!--================Header Menu Area =================-->
+	<header class="header_area">
+		
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="index.html">
+							<b>SECOUR HEALTH</b>
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+					 aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<div class="row ml-0 w-100">
+							<div class="col-lg-12 pr-0">
+							
+								<ul class="nav navbar-nav center_nav pull-right">
+									<li class="nav-item active">
+										<a class="nav-link" href="adminhome.php">HOME</a>
+									</li>
+									
+									<li class="nav-item submenu dropdown">
+										<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">REGISTRATION</a>
+										<ul class="dropdown-menu">
+											<li class="nav-item">
+												<a class="nav-link" href="doctorreg.php">Doctor</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="departmentreg.php">Department</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="surveyreg.php">Survay</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="panchayathreg.php">Panchayath</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="question.php">Questions</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="healthinsurance.php">Health Insurance</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="healthservice.php">Health Services</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="ashaworkerreg.php">Asha Worker</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="ashaworkerallocation.php">Work Allocation</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item submenu dropdown">
+										<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">VIEW</a>
+										<ul class="dropdown-menu">
+											<li class="nav-item">
+												<a class="nav-link" href="viewpanchayath.php">Panchayath</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="single-blog.html">Survay Details</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="single-blog.html">Summary</a>
+
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="viewcomplaint.php">Complaints</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="viewfeedback.php">Feedback</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="viewinsurapplieduser.php">Insurance applied User</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="viewhealthservice.php">Service</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="login.php">Logout</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!--================Header Menu Area =================-->
+
+	<!--================ Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner d-flex align-items-center">
+			<div class="container">
+				<div class="banner_content row">
+					<div class="col-lg-12">
+						<h1>We Care for Your Health Every Moment</h1>
+						<p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price You may see some
+							for as low as each.</p>
+						<a class="main_btn mr-10" href="#">get started</a>					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<br>
+	<br>
+	<br>
