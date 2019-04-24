259c12692a766c5b64669f89fe864c1354f60b7c
Vindhujavenu <vindhuja@94venu>
2019-04-24 19:47:56 +0530

    Update view_health_request.php

--- android_connection/view_health_request.php
+++ android_connection/view_health_request.php
@@ -4,7 +4,9 @@ $response=array();
 
 $lid=$_GET['lid'];
 $hid=$_GET['hid'];
-$qry=mysql_query("select family.*,health_insurance_application.* from family,health_insurance_application,asha_worker,member where asha_worker.log_id='$lid' and asha_worker.p_id=family.pid and health_insurance_application.hid='$hid' and health_insurance_application.status='pending' and health_insurance_application.log_id=member.log_id and member.f_id=family.f_id");
+//$lid="37";
+//$hid="9";
+$qry=mysql_query("select family.*,health_insurance_application.* from family,health_insurance_application,asha_worker,member where asha_worker.log_id='$lid' and asha_worker.p_id=family.pid and  family.f_id=member.f_id and health_insurance_application.hid='$hid' and health_insurance_application.status='pending' and health_insurance_application.log_id=member.log_id");
 if(mysql_num_rows($qry)>0)
 {
 	$response["status"]="1";
@@ -12,10 +14,10 @@ if(mysql_num_rows($qry)>0)
 	while($b=mysql_fetch_array($qry))
 	{
 		$arr=array();
-		$arr["name"]=$b[3];
-		$arr["income"]=$b[4];
-		$arr["members"]=$b[5];
-		$arr["rid"]=$b[8];
+		$arr["hname"]=$b[2];
+		$arr["hno"]=$b[1];
+		$arr["owner"]=$b[3];
+		$arr["rid"]=$b['appid'];
 		array_push($response["result"],$arr);
 	}
 }
