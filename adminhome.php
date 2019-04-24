77ff8c79ad293fd2c94f124bd788b7952969195e
Vindhujavenu <vindhuja@94venu>
2019-04-24 13:02:44 +0530

    Update adminhome.php

--- adminhome.php
+++ adminhome.php
@@ -4,59 +4,15 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 </head>
-
+<?php
+include("header.php");
+?>
 <body>
 <form id="form1" name="form1" method="post" action="">
-  <table width="224" border="1">
-    <tr>
-      <td><a href="doctorreg.php">Doctor Registration</a></td>
-    </tr>
-    <tr>
-      <td><a href="departmentreg.php">Department Registration</a></td>
-    </tr>
-    <tr>
-      <td width="214"><a href="surveyreg.php">Survey Registration</a></td>
-    </tr>
-    <tr>
-      <td><a href="panchayathreg.php">Panchayath Registration</a></td>
-    </tr>
-    <tr>
-      <td><a href="viewpanchayath.php">View Panchayath</a></td>
-    </tr>
-    <tr>
-      <td><a href="question.php">Question Mangement</a></td>
-    </tr>
-    <tr>
-      <td><a href="ashaworkerreg.php">Asha-Worker-Registration</a></td>
-    </tr>
-    <tr>
-      <td><a href="ashaworkerallocation.php">Asha-Workers Work -Allocation</a></td>
-    </tr>
-    <tr>
-      <td><a href="">View Survey Details</a></td>
-    </tr>
-    <tr>
-      <td><a href="#">Publish Survey Summary</a></td>
-    </tr>
-    <tr>
-      <td><a href="#">Chat with Doctors</a></td>
-    </tr>
-    <tr>
-      <td><a href="viewcomplaint.php">View Complaints & Send Reply</a></td>
-    </tr>
-    <tr>
-      <td><a href="viewfeedback.php">View Feedback</a></td>
-    </tr>
-    <tr>
-      <td><a href="healthinsurance.php">Add Health Insurance</a></td>
-    </tr>
-    <tr>
-      <td><a href="viewinsurapplieduser.php">Insurance-Applied-Users</a></td>
-    </tr>
-    <tr>
-      <td><a href="healthservice.php">Health Service</a></td>
-    </tr>
-  </table>
+ 
 </form>
 </body>
 </html>
+<?php
+include("footer.php");
+?>
