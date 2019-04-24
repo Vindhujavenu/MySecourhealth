9ada27dd80cd38801dea0c46606c48093842147e
Vindhujavenu <vindhuja@94venu>
2019-04-24 13:03:49 +0530

    Update ajaxviewinsurappluser.php

--- ajaxviewinsurappluser.php
+++ ajaxviewinsurappluser.php
@@ -5,7 +5,9 @@
   <tr></tr>
   <tr>
     <td width="54"><div align="center">SlNo</div></td>
-    <td width="85"><div align="center">User-Name</div></td>
+	   <td width="85"><div align="center">House Number</div></td>
+	      <td width="85"><div align="center">House Name</div></td>
+    <td width="85"><div align="center">Owner Name</div></td>
     <td width="43"><div align="center">File</div></td>
     <td width="80"><div align="center">Date</div></td>
     <td width="38"><div align="center">Status</div></td>
@@ -13,8 +15,8 @@
   </tr>
   <?php
 	include("connection.php");
-	$id=$_GET['hinsur'];
-	$a=mysql_query("select health_insurance_application.*,patient.name,health_insurance.file from health_insurance_application,patient,health_insurance where health_insurance_application.log_id=patient.log_id and health_insurance_application.hid=health_insurance.hid");
+	$id=$_GET['id'];
+	$a=mysql_query("select health_insurance_application.*,family.house_no,family.house_name,family.owner_name,health_insurance.file from health_insurance_application,family,health_insurance,member where health_insurance_application.log_id=member.log_id and member.f_id=family.f_id and health_insurance_application.hid=health_insurance.hid and health_insurance_application.hid='$id'");
 	$i=1;
 	while($b=mysql_fetch_array($a))
 	{
@@ -23,8 +25,10 @@
 	?>
   <tr>
     <td><?php echo $i++ ?></td>
-    <td><?php echo $b[6] ?></td>
-    <td><?php echo $b[7] ?></td>
+	 <td><?php echo $b[6] ?></td>
+	  <td><?php echo $b[7] ?></td>
+    <td><?php echo $b[8] ?></td>
+    <td><img src="file/<?php echo $b[9] ?>" height="100"width="100"></img></td>
     <td><?php echo $b[2] ?></td>
     <td><?php echo $b[4] ?></td>
   </tr>
