64b38baeaf14e47ce251e01d66c695ba9a5a1631
Vindhujavenu <vindhuja@94venu>
2019-04-24 19:45:40 +0530

    Update userfeedback.php

--- android_connection/userfeedback.php
+++ android_connection/userfeedback.php
@@ -1,16 +1,17 @@
 <?php
 include("connection.php");
+$response=array();
 $feedback=$_GET['feedback'];
 $lid=$_GET['lid'];
-$response=array();
-$gry=mysql_query("insert into feedback(log_id,feedback,date)values('$lid','$feedback',curdate())");
-if($qry>0)
+
+$res=mysql_query("insert into feedback values(null,'$lid','$feedback',curdate())");
+if($res>0)
 {
-	$response['status']="1";
+	$response["status"]="1";
 }
 else
 {
-	$response['status']="0";
+	$response["status"]="0";
 }
 echo json_encode($response);
 ?>
